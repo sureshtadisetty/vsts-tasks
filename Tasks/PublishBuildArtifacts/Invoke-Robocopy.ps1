9ec8bef6c57579f53fa7cc26c5e90170ac96062f
Eric Sciple <ersciple@microsoft.com>
2016-08-25 00:55:55 -0400

    .

---
+++ Tasks/PublishBuildArtifacts/Invoke-Robocopy.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$Source,
+
+    [Parameter(Mandatory = $true)]
+    [string]$Target)
+
+# This script translates the output from robocopy into UTF8.
+#
+# Node has limited support for encodings, and robocopy writes output using the
+# default console-output code page. For example, on an en-US box, this is code
+# page '437', which nodejs does not support.
+#
+# On an en-US box, testing with the 'ç' character is a good way to determine
+# whether the data is passed correctly between processes. This is because the
+# 'ç' character has a different code point across each of the common encodings
+# on an en-US box:
+#   1) the system default code page (i.e. CP_ACP) (Windows-1252)
+#   2) the default console-output code page (IBM437)
+#   3) UTF8
+
+$ErrorActionPreference = 'Stop'
+
+# Redefine the wrapper over STDOUT to use UTF8. Node expects UTF8 by default.
+$stdout = [System.Console]::OpenStandardOutput()
+$utf8 = [System.Text.Encoding]::UTF8
+$writer = New-Object System.IO.StreamWriter($stdout, $utf8)
+[System.Console]::SetOut($writer)
+
+# Print the ##command.
+"##[command]robocopy.exe /E /COPY:DAT /XA:H /NP /R:3 `"$Source`" `"$Target`" *"
+
+# Robocopy writes output using the default console-output code page. Powershell
+# by default expects external commands to use the default console-output code page.
+& robocopy.exe /E /COPY:DAT /XA:H /NP /R:3 $Source $Target *
+"##[debug]exit code '$LASTEXITCODE'"
+if ($LASTEXITCODE -ge 8) {
+    exit $LASTEXITCODE
+}
--- Tasks/PublishBuildArtifacts/publishbuildartifacts.ts
+++ Tasks/PublishBuildArtifacts/publishbuildartifacts.ts
@@ -1,72 +1,92 @@
+/// <reference path="../../definitions/Q.d.ts" />
 /// <reference path="../../definitions/vsts-task-lib.d.ts" />
 
 import os = require('os');
 import path = require('path');
+import process = require('process');
 import tl = require('vsts-task-lib/task');
 import tr = require('vsts-task-lib/toolrunner');
 
-tl.setResourcePath(path.join(__dirname, 'task.json'));
+// used for escaping file paths that are passed into the powershell command
+let pathToPSString = (filePath: string) => {
+    let result: string =
+        filePath.replace(/"/g, '') // remove double quotes
+        .replace(/'/g, "''"); // double-up single quotes
+    return `'${result}'`; // enclose in single quotes
+}
 
-// content is a folder contain artifacts needs to publish.
-let pathtoPublish: string = tl.getPathInput('PathtoPublish', true, true);
-let artifactName: string = tl.getInput('ArtifactName', true);
-let artifactType: string = tl.getInput('ArtifactType', true);
+async function run() {
+    try {
+        tl.setResourcePath(path.join(__dirname, 'task.json'));
 
-artifactType = artifactType.toLowerCase();
+        // PathtoPublish is a folder that contains the files
+        let pathtoPublish: string = tl.getPathInput('PathtoPublish', true, true);
+        let artifactName: string = tl.getInput('ArtifactName', true);
+        let artifactType: string = tl.getInput('ArtifactType', true);
 
-try {
-    let data = {
-        artifacttype: artifactType,
-        artifactname: artifactName
-    };
+        artifactType = artifactType.toLowerCase();
+        let data = {
+            artifacttype: artifactType,
+            artifactname: artifactName
+        };
 
-    // upload or copy
-    if (artifactType === "container") {
-        data["containerfolder"] = artifactName;
+        // upload or copy
+        if (artifactType === "container") {
+            data["containerfolder"] = artifactName;
 
-        // add localpath to ##vso command's properties for back compat of old Xplat agent
-        data["localpath"] = pathtoPublish;
-        tl.command("artifact.upload", data, pathtoPublish);
-    }
-    else if (artifactType === "filepath") {
-        let targetPath: string = tl.getInput('TargetPath', true);
-        let artifactPath: string = path.join(targetPath, artifactName);
-        tl.mkdirP(artifactPath);
+            // add localpath to ##vso command's properties for back compat of old Xplat agent
+            data["localpath"] = pathtoPublish;
+            tl.command("artifact.upload", data, pathtoPublish);
+        }
+        else if (artifactType === "filepath") {
+            let targetPath: string = tl.getInput('TargetPath', true);
+            let artifactPath: string = path.join(targetPath, artifactName);
+            tl.mkdirP(artifactPath);
 
-        if (os.platform() == 'win32') {
-            // create the artifact. at this point, mkdirP already succeeded so the path is good.
-            // the artifact should get cleaned up during retention even if the copy fails in the
-            // middle
-            tl.command("artifact.associate", data, targetPath);
+            if (os.platform() == 'win32') {
+                // create the artifact. at this point, mkdirP already succeeded so the path is good.
+                // the artifact should get cleaned up during retention even if the copy fails in the
+                // middle
+                tl.command("artifact.associate", data, targetPath);
 
-            // copy the files
-            let robocopy: tr.ToolRunner = new tr.ToolRunner('robocopy');
-            robocopy.arg('/E'); // copy subdirectories, including Empty ones.
-            robocopy.arg('/NP'); // No Progress - don't display percentage copied.
-            robocopy.arg('/R:3'); // number of Retries on failed copies
-            robocopy.arg(pathtoPublish); // source
-            robocopy.arg(artifactPath); // destination
-            robocopy.arg('*'); // file
-            let execOptions: tr.IExecOptions = { ignoreReturnCode: true };
-            let result: tr.IExecResult = robocopy.execSync(execOptions);
-            if (result.code >= 8) {
-                tl.setResult(tl.TaskResult.Failed, tl.loc('RobocopyFailed', result.code));
+                // copy the files
+                let script: string = path.join(__dirname, 'Invoke-Robocopy.ps1');
+                let command: string = `& ${pathToPSString(script)} -Source ${pathToPSString(pathtoPublish)} -Target ${artifactPath}`
+                let powershell: tr.ToolRunner = new tr.ToolRunner('powershell.exe');
+                powershell.arg('-NoLogo');
+                powershell.arg('-Sta');
+                powershell.arg('-NoProfile');
+                powershell.arg('-NonInteractive');
+                powershell.arg('-ExecutionPolicy');
+                powershell.arg('Unrestricted');
+                powershell.arg('-Command');
+                powershell.arg(command);
+                powershell.on('stdout', (buffer: Buffer) => {
+                    process.stdout.write(buffer);
+                });
+                powershell.on('stderr', (buffer: Buffer) => {
+                    process.stderr.write(buffer);
+                });
+                let execOptions: tr.IExecOptions = { silent: true };
+                await powershell.exec(execOptions);
+            }
+            else {
+                // log if the path does not look like a UNC path (artifact creation will fail)
+                if (!artifactPath.startsWith('\\\\') || artifactPath.length < 3) {
+                    console.log(tl.loc('UncPathRequired'));
+                }
+
+                // create the artifact
+                data['artifactlocation'] = targetPath; // artifactlocation for back compat with old xplat agent
+                tl.command("artifact.associate", data, targetPath);
+
+                console.log(tl.loc('SkippingCopy')); // add fwlink to message
             }
         }
-        else {
-            // log if the path does not look like a UNC path (artifact creation will fail)
-            if (!artifactPath.startsWith('\\\\') || artifactPath.length < 3) {
-                console.log(tl.loc('UncPathRequired'));
-            }
-
-            // create the artifact
-            data['artifactlocation'] = targetPath; // artifactlocation for back compat with old xplat agent
-            tl.command("artifact.associate", data, targetPath);
-
-            console.log(tl.loc('SkippingCopy')); // add fwlink to message
-        }
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, tl.loc('PublishBuildArtifactsFailed', err.message));
     }
 }
-catch (err) {
-    tl.setResult(tl.TaskResult.Failed, tl.loc('PublishBuildArtifactsFailed', err.message));
-}
+
+run();
