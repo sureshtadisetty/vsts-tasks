f59580d0457ed1eb91541dfcbb42c8ba6955db88
Eric Sciple <ersciple@microsoft.com>
2016-08-25 23:19:40 -0400

    .

--- Tasks/PublishBuildArtifacts/Invoke-Robocopy.ps1
+++ Tasks/PublishBuildArtifacts/Invoke-Robocopy.ps1
@@ -8,12 +8,15 @@ param(
 
 # This script translates the output from robocopy into UTF8.
 #
-# Node has limited support for encodings, and robocopy writes output using the
-# default console-output code page. For example, on an en-US box, this is code
-# page '437', which nodejs does not support.
+# Robocopy does not respect the active console output code page for a process
+# (set using chcp.com) and instead appears to always follow the higher level
+# global setting. For example, on an en-US box, this would be the code page '437'.
+#
+# Node has limited built-in support for encodings. For example, does not support
+# code page 437.
 #
 # On an en-US box, testing with the 'ç' character is a good way to determine
-# whether the data is passed correctly between processes. This is because the
+# whether data is passed correctly between processes. This is because the
 # 'ç' character has a different code point across each of the common encodings
 # on an en-US box:
 #   1) the system default code page (i.e. CP_ACP) (Windows-1252)
@@ -24,7 +27,7 @@ $ErrorActionPreference = 'Stop'
 
 # Redefine the wrapper over STDOUT to use UTF8. Node expects UTF8 by default.
 $stdout = [System.Console]::OpenStandardOutput()
-$utf8 = [System.Text.Encoding]::UTF8
+$utf8 = New-Object System.Text.UTF8Encoding($false) # do not emit BOM
 $writer = New-Object System.IO.StreamWriter($stdout, $utf8)
 [System.Console]::SetOut($writer)
 
